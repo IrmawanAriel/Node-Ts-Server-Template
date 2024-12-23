8c7c52e12468ccb1e389ae1e0b15af64095a387c
unknown <ariel.irmawan@aqi.co.id>
2024-12-23 10:24:03 +0700

    boiler is done

--- src/Routers/mail.ts
+++ src/Routers/mail.ts
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Response, Request, Router } from "express";
 
 const mailingrouter = Router();
 
-mailingrouter.get('/mailing', ()=> {
-    console.log('mailing route accessed');
+mailingrouter.get('/get', (req: Request, res: Response) => {
+    res.json({ message: 'Welcome to the API!' });
 })
 
 export default mailingrouter;
